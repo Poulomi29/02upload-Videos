a727508d55bc0523f7e087189d827d2865c1d28a
roypoulomi <poulomi@check-n-click.com>
2021-01-21 06:16:31 +0000

    Copied the code from Rebecca

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
+require("dotenv").config();
 var cloudinary = require('cloudinary').v2;
-
 cloudinary.uploader
   .upload('Basic_Tee_Clip_2x.mp4', 
   {
@@ -8,7 +8,6 @@ cloudinary.uploader
     public_id: 'Basic_Tee', 
     folder: "Cloudinary-Partner-Training/eCommerce-videos",     
     tags:["position_1","whiteTee","pwg01"],       
-    
     overwrite:"true"    
   })
   .then(result => {
